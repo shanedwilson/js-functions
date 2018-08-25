85a64165e5560e46382d82565bd59507a2bf572f
Shane D Wilson <me@shanedwilson.com>
2018-08-25 10:27:19 -0500

    object shorthand

--- main.js
+++ main.js
@@ -15,3 +15,17 @@ const firstName = 'Shane'
 const lastName = 'Wilson'
 //greetingElement.innerHTML = 'Hello my name is ' + firstName + ' ' + lastName;
 greetingElement.innerHTML = `Hello my name is ${firstName} ${lastName}`;
+
+
+//object shorthand notation
+let name = 'Shane';
+let age = '48';
+
+// let person = {
+//     name: name,
+//     age: age,
+// };
+
+let person = {name,age};
+
+console.log('person:', person.name);
