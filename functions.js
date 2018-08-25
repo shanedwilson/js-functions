4825735814bc2e8d56e590b415450a5f03146463
Shane D Wilson <me@shanedwilson.com>
2018-08-25 11:48:35 -0500

    created writeToDom

--- functions.js
+++ functions.js
@@ -18,6 +18,28 @@ const bearSticks = nuggetizer('bear');
 
 const nomnom =(name, food) => {
     return `${name} devoured ${food}`
-}
+};
 console.log(nomnom('shane', bearSticks)); //shane devoured bear stix
 console.log(nomnom('stacye', 'blackbeans'));
+
+//function numberAdder that takes in a number and 
+//returns the number plus 3. ex i/p 22 o/p 25
+
+const numberAdder = (num) => {
+    const finalNumber =  num + 3;
+    printToDom(`<h2>${finalNumber}</h2>`, 'allTheNumbers')
+};
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+
+numberAdder(22);
+numberAdder(2);
+numberAdder(4);
+numberAdder(6);
+numberAdder(12);
+
+
+printToDom('I am ready for lunch', 'feelings');
